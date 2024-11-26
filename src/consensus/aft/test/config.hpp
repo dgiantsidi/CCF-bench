72599392df3a988dc64825ace963f3a9f9fc99f8
dgiantsidi <dimitra.giantsidi@gmail.com>
2024-11-26 14:34:16 +0000

    .

--- src/consensus/aft/test/config.hpp
+++ src/consensus/aft/test/config.hpp
@@ -3,6 +3,6 @@
 #include "consensus/aft/raft.h"
 #include "consensus/aft/raft_types.h"
 
-constexpr int k_num_requests = 4; // 500000;
+constexpr int k_num_requests = 400000; // 500000;
 constexpr int payload_sz = sizeof(size_t);
 constexpr int payload_sz_entry = sizeof(size_t) + sizeof(bool)+ sizeof(ccf::kv::Term) + sizeof(ccf::kv::Version); //<payload_size><term><kv_version>;
