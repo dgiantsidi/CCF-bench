b6dc83899852987c2bb8530354e1587c650e3477
dgiantsidi <dimitra.giantsidi@gmail.com>
2024-11-27 15:38:07 +0000

    .

--- src/consensus/aft/test/committable_suffix.cpp
+++ src/consensus/aft/test/committable_suffix.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache 2.0 License.
-
+#if 0
 #include "test_common.h"
 
 #define DOCTEST_CONFIG_NO_SHORT_MACRO_NAMES
@@ -1012,3 +1012,4 @@ DOCTEST_TEST_CASE_TEMPLATE("Multi-term divergence", T, WorstCase, RandomCase)
     }
   }
 }
+#endif
