524a0c0d4ecab22bf04ceaa8ec22997be1582afb
dgiantsidi <dimitra.giantsidi@gmail.com>
2024-12-03 18:18:10 +0000

    .

--- src/consensus/aft/test/message_queue.h
+++ src/consensus/aft/test/message_queue.h
@@ -23,13 +23,13 @@ public:
     if (dq.empty())
     {
       fmt::print("{} --> no elem\n", __func__);
-      return {ccf::NodeId("0"), std::make_unique<uint8_t[]>(1), 0};
+      return {ccf::NodeId(0), std::make_unique<uint8_t[]>(1), 0};
     }
     fmt::print("{}\n", __func__);
     std::unique_ptr<message>& front = dq.front();
     std::unique_ptr<uint8_t[]> ret_msg = std::move(front->msg);
     size_t ret_sz = front->msg_sz;
-    ccf::NodeId node_id = front->node_id;
+    ccf::NodeId node_id = ccf::NodeId(0); //front->node_id;
     dq.pop_front();
     fmt::print("{} ---\n", __func__);
     return {node_id, std::move(ret_msg), ret_sz};
