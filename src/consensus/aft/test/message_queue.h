b7bd7143e67bd74f75b750da4d91afffdbc17afa
dgiantsidi <dimitra.giantsidi@gmail.com>
2024-12-04 13:41:45 +0000

    .

--- src/consensus/aft/test/message_queue.h
+++ src/consensus/aft/test/message_queue.h
@@ -5,23 +5,51 @@
 #include <memory>
 #include <mutex>
 #include <tuple>
+#include <fmt/printf.h>
 
 // thread-safe
 class m_queue
 {
 public:
+  m_queue() {
+    fmt::print("{} \n", __func__);
+  }
+
+  m_queue(const m_queue& other) {
+    fmt::print("{} \n", __func__);
+  }
+
+  m_queue(m_queue&& other) {
+    fmt::print("{} \n", __func__);
+  }
+
   void append(ccf::NodeId node_id, std::unique_ptr<uint8_t[]> msg, size_t size)
   {
-    std::unique_lock<std::mutex> tmp_lock(dq_mtx);
-    fmt::print("{}\n", __func__);
-    dq.emplace_back(std::make_unique<message>(node_id, std::move(msg), size));
+    fmt::print("{} --- 0\n", __func__);
+
+    //std::lock_guard<std::mutex> tmp_lock(dq_mtx);
+    fmt::print("{} --- 1\n", __func__);
+    auto ptr = std::make_unique<message>(node_id, std::move(msg), size);
+    fmt::print("{} --- 2\n", __func__);
+    dq.push_back(std::move(ptr));
+  }
+
+  void append(std::unique_ptr<uint8_t[]> msg, size_t size)
+  {
+    fmt::print("{} --- 0\n", __func__);
+
+    //std::lock_guard<std::mutex> tmp_lock(dq_mtx);
+    fmt::print("{} --- 1\n", __func__);
+    auto ptr = std::make_unique<message>(std::move(msg), size);
+    fmt::print("{} --- 2\n", __func__);
+    dq.push_back(std::move(ptr));
   }
 
   std::tuple<ccf::NodeId, std::unique_ptr<uint8_t[]>, size_t> pop()
   {
     fmt::print("{} \n", __PRETTY_FUNCTION__);
 
-    std::unique_lock<std::mutex> tmp_lock(dq_mtx);
+    //std::lock_guard<std::mutex> tmp_lock(dq_mtx);
     if (dq.empty())
     {
       fmt::print("{} --> no elem\n", __func__);
@@ -29,13 +57,15 @@ public:
     }
 
     fmt::print("{}1\n", __func__);
-    std::unique_ptr<message>& front = dq.front();
+
+    std::unique_ptr<message> front = std::move(dq.front());
+
     fmt::print("{}2\n", __func__);
 
-    std::unique_ptr<uint8_t[]> ret_msg = std::make_unique<uint8_t[]>(front->msg_sz);// std::move(front->msg);
+    //std::unique_ptr<uint8_t[]> ret_msg = std::make_unique<uint8_t[]>(front->msg_sz);// std::move(front->msg);
     fmt::print("{}3\n", __func__);
 
-    size_t ret_sz = front->msg_sz;
+    //size_t ret_sz = front->msg_sz;
     fmt::print("{}4\n", __func__);
 
     //ccf::NodeId node_id = ccf::NodeId(0); // front->node_id;
@@ -43,7 +73,9 @@ public:
 
     //dq.pop_front();
     fmt::print("{} ---\n", __func__);
-    return {ccf::NodeId(0), std::move(ret_msg), ret_sz};
+          return {ccf::NodeId(0), std::make_unique<uint8_t[]>(1), 0};
+
+//    return {ccf::NodeId(0), std::move(ret_msg), ret_sz};
   }
 
 private:
@@ -55,14 +87,29 @@ private:
     explicit message(
       ccf::NodeId _node_id, std::unique_ptr<uint8_t[]> _msg, size_t _msg_sz)
     {
+      fmt::print("{}\n", __PRETTY_FUNCTION__);
+
       node_id = _node_id;
       msg = std::move(_msg);
       msg_sz = _msg_sz;
     }
+
+explicit message(
+     std::unique_ptr<uint8_t[]> _msg, size_t _msg_sz)
+    {
+      fmt::print("{}\n", __PRETTY_FUNCTION__);
+
+     
+      msg = std::move(_msg);
+      msg_sz = _msg_sz;
+    }
+
     message() = delete;
 
     message(const message& other)
     {
+            
+
       fmt::print("{} \n", __PRETTY_FUNCTION__);
       node_id = other.node_id;
       msg_sz = other.msg_sz;
@@ -81,11 +128,13 @@ private:
 
     message& operator=(const message& other)
     {
-      fmt::print("{} \n", __PRETTY_FUNCTION__);
-      node_id = other.node_id;
-      msg_sz = other.msg_sz;
-      msg = std::make_unique<uint8_t[]>(other.msg_sz);
-      ::memcpy(msg.get(), other.msg.get(), msg_sz);
+      if (this != &other) {
+        fmt::print("{} \n", __PRETTY_FUNCTION__);
+        node_id = other.node_id;
+        msg_sz = other.msg_sz;
+        msg = std::make_unique<uint8_t[]>(other.msg_sz);
+        ::memcpy(msg.get(), other.msg.get(), msg_sz);
+      }
       return *this;
     }
 
@@ -99,6 +148,7 @@ private:
       return *this;
     }
   };
+
   std::deque<std::unique_ptr<message>> dq;
   std::mutex dq_mtx;
 };
---
+++ src/consensus/aft/test/test_message_queue.cpp
@@ -0,0 +1,56 @@
+#include "message_queue.h"
+
+#include <thread>
+#include <vector>
+std::shared_ptr<m_queue> Q;
+
+// static void run_func_append(std::shared_ptr<m_queue> q) {
+static void run_func_append()
+{
+  fmt::print("{}\n", __func__);
+
+  for (auto i = 0ULL; i < 10000; i++)
+  {
+    size_t msg_sz = 10;
+    auto ptr = std::make_unique<uint8_t[]>(msg_sz);
+    fmt::print("{} i = {}\n", __func__, i);
+
+    Q->append(ccf::NodeId(0), std::move(ptr), msg_sz);
+  }
+  fmt::print("{}\n", __func__);
+}
+
+// static void run_func_pop(std::shared_ptr<m_queue> q) {
+static void run_func_pop()
+{
+  int count = 0;
+  for (;;)
+  {
+    auto [n_id, data, data_sz] = Q->pop();
+    if (data_sz > 0)
+    {
+      count++;
+    }
+    if (count == 10e6)
+      return;
+  }
+}
+
+int main()
+{
+  std::vector<std::thread> vec_threads;
+  Q = std::shared_ptr<m_queue>();
+  // std::shared_ptr<m_queue> q = std::make_shared<m_queue>();
+  run_func_append();
+#if 0
+    for (auto i = 0ULL; i < 1; i++) {
+        vec_threads.emplace_back(std::thread(run_func_append));
+        
+    }
+#endif
+  vec_threads[0].join();
+  // vec_threads.emplace_back(std::thread(run_func_pop, q));
+  // vec_threads[1].join();
+
+  return 0;
+}
