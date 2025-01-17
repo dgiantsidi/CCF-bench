4e61e43988a8c26d7f2dbddd9840e22635303bb3
dgiantsidi <dimitra.giantsidi@gmail.com>
2025-01-17 16:51:30 +0000

    .

--- src/consensus/aft/test/config.hpp
+++ src/consensus/aft/test/config.hpp
@@ -9,7 +9,8 @@ constexpr int payload_sz_entry = sizeof(size_t) + sizeof(bool) +
   sizeof(ccf::kv::Term) +
   sizeof(ccf::kv::Version); //<payload_size><term><kv_version>;
 
-#ifdef REGURAL_VM
+#ifdef REGULAR_VM
+#warning "R-VMs running here"
 constexpr int primary_node = 0;
 std::string primary_ip = "10.5.0.7"; // 10.5.0.7";
 constexpr int primary_listening_port = 1800;
