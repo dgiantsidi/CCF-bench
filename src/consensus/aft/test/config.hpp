70e3797d66bf0cf393b2e1e27571886be653f7d9
dgiantsidi <dimitra.giantsidi@gmail.com>
2025-01-17 14:45:29 +0000

    removed unused entry

--- src/consensus/aft/test/config.hpp
+++ src/consensus/aft/test/config.hpp
@@ -9,7 +9,6 @@ constexpr int payload_sz_entry = sizeof(size_t) + sizeof(bool) +
   sizeof(ccf::kv::Term) +
   sizeof(ccf::kv::Version); //<payload_size><term><kv_version>;
 
-constexpr int total_nodes = 3;
 constexpr int primary_node = 0;
 std::string primary_ip = "10.5.0.7";
 constexpr int primary_listening_port = 1800;
