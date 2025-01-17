280ee11aff5e3c78b8bfda23437aec5017f1884d
dgiantsidi <dimitra.giantsidi@gmail.com>
2025-01-17 15:03:28 +0000

    .

--- src/consensus/aft/test/driver.cpp
+++ src/consensus/aft/test/driver.cpp
@@ -56,11 +56,10 @@ void callable_obj_replication(
     auto data = std::make_shared<std::vector<uint8_t>>();
     std::shared_ptr<RaftDriver> raft_drv =
       std::static_pointer_cast<RaftDriver>(drv_shared);
-    // std::cout << __PRETTY_FUNCTION__ << " committed_seqno=" <<
-    // raft_drv->get_committed_seqno() << "\n";
+    
     reqs_no++;
-    auto now_ts = get_timestamp_ns();
-    if (reqs_no % 10000 == 0)
+   // auto now_ts = get_timestamp_ns();
+    if (reqs_no % 50000 == 0)
     {
       std::cout << __PRETTY_FUNCTION__
                 << " committed_seqno=" << raft_drv->get_committed_seqno()
@@ -78,10 +77,10 @@ void callable_obj_replication(
                 << "\n";
 #endif
     }
-    auto end_ts = get_timestamp_ns();
-    std::cout << __PRETTY_FUNCTION__ << " reqs_no=" << reqs_no
-              << " committed_seqno=" << raft_drv->get_committed_seqno()
-              << " latency (ns)=" << (end_ts - now_ts) << "\n";
+    // auto end_ts = get_timestamp_ns();
+    // std::cout << __PRETTY_FUNCTION__ << " reqs_no=" << reqs_no
+    //          << " committed_seqno=" << raft_drv->get_committed_seqno()
+    //          << " latency (ns)=" << (end_ts - now_ts) << "\n";
   }
   else
   {
@@ -154,11 +153,13 @@ static void apply_cmds(std::shared_ptr<RaftDriver> driver)
       // fmt::print("{} --> data_sz={}\n", __func__, data_sz);
       auto src_node_str = ccf::NodeId(std::to_string(src_node));
       driver->periodic_applying(src_node_str, data.get(), data_sz);
+      #if 0
       fmt::print(
         "{} src_node={}, cmt_idx={} \n",
         __func__,
         src_node_str,
         driver->get_committed_seqno());
+        #endif
     }
     else if (data_sz == 0)
     {
