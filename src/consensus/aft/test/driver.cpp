79b80e67e4c8ae99325b8d39ab45674de9ca5a28
dgiantsidi <dimitra.giantsidi@gmail.com>
2026-02-04 19:45:41 +0000

    udpates on driver

--- src/consensus/aft/test/driver.cpp
+++ src/consensus/aft/test/driver.cpp
@@ -8,13 +8,13 @@
 #include "ccf/ds/hash.h"
 #include "config.hpp"
 #include "networking_api.h"
-
 #include "ngtcp2-unmodified/examples/ccf_related_work/config.h"
-//#warning "Linking with server_req_pipelining.h"
-//#include "ngtcp2/examples/server_req_pipelining.h"
-#include "ngtcp2-unmodified/examples/ccf_related_work/server_ccf.h"
+// #warning "Linking with server_req_pipelining.h"
+// #include "ngtcp2/examples/server_req_pipelining.h"
+#include "ngtcp2-unmodified/examples/ccf_related_work/server_ccf_multitenant.h"
 #include "ngtcp2-unmodified/examples/template.h"
 #include "ngtcp2-unmodified/examples/util.h"
+
 #include <arpa/inet.h>
 #include <cassert>
 #include <chrono>
@@ -22,12 +22,11 @@
 #include <iostream>
 #include <regex>
 #include <string>
+#include <barrier>
+
 
 using namespace std;
 std::mutex leader_mtx;
-
-
-
 
 struct metadata
 {
@@ -37,7 +36,7 @@ struct metadata
   metadata() = delete;
   explicit metadata(uint64_t req, uint64_t lat) :
     client_req_id(req),
-    latency_ns(lat){};
+    latency_ns(lat) {};
 
   metadata(const metadata& other)
   {
@@ -109,18 +108,16 @@ void empty_func(
   std::weak_ptr<void> driver,
   uint64_t client_req_id,
   uint8_t* data = nullptr,
-  size_t sz_data = 0
-  )
+  size_t sz_data = 0)
 {}
 
-
-uint64_t ccf_committed_seqno(
-  std::weak_ptr<void> driver) {
+uint64_t ccf_committed_seqno(std::weak_ptr<void> driver)
+{
   std::shared_ptr<void> drv_shared = driver.lock();
   if (drv_shared)
   {
     std::shared_ptr<RaftDriver> raft_drv =
-    std::static_pointer_cast<RaftDriver>(drv_shared);
+      std::static_pointer_cast<RaftDriver>(drv_shared);
     return raft_drv->get_committed_seqno();
   }
   else
@@ -149,13 +146,14 @@ void ccf_replication(
     auto data_to_replicate = std::make_shared<std::vector<uint8_t>>(sz_data);
     ::memcpy(data_to_replicate->data(), data, sz_data);
     std::shared_ptr<RaftDriver> raft_drv =
-    std::static_pointer_cast<RaftDriver>(drv_shared);
-
+      std::static_pointer_cast<RaftDriver>(drv_shared);
+    // std::cout << "sz_data=" << sz_data << "B\n"; // debug: should be 40B (8+32 commitments)
     reqs_no++;
-    uint64_t zil_blk_id = 2;// std::stoll;
+    uint64_t zil_blk_id = 2; // std::stoll;
     ::memcpy(&zil_blk_id, data_to_replicate->data(), sizeof(uint64_t));
     auto now_ts = get_timestamp_ns();
-    if (client_req_id <= raft_drv->get_committed_seqno()) {
+    if (client_req_id <= raft_drv->get_committed_seqno())
+    {
       std::cout << "*====RAFT====* " << __PRETTY_FUNCTION__
                 << " client_req_id=" << client_req_id
                 << " zil_blk_id=" << zil_blk_id
@@ -163,14 +161,14 @@ void ccf_replication(
                 << " ERROR\n";
       exit(-1);
     }
-    if (client_req_id % 10000 ==0) {
+    if (client_req_id % 10000 == 0)
+    {
       std::cout << "*====RAFT====* " << __PRETTY_FUNCTION__
                 << " client_req_id=" << client_req_id
-                << " zil_blk_id=" << zil_blk_id
-                << " size=" << sz_data
+                << " zil_blk_id=" << zil_blk_id << " size=" << sz_data
                 << " committed_seqno=" << raft_drv->get_committed_seqno()
                 << "\n";
-   }
+    }
     raft_drv->replicate_commitable("2", data_to_replicate, 0);
     if (reqs_no % 50000 == 0)
     {
@@ -192,9 +190,8 @@ void ccf_replication(
       // std::cout << latencies;
       latencies.clear();
     }
-    
 
-   #if 0
+#if 0
     // wait until committed
     while (raft_drv->get_committed_seqno() < reqs_no)
     {
@@ -207,18 +204,20 @@ void ccf_replication(
                 << "After replicate_commitable: reqs_no=" << reqs_no
                 << " committed_seqno=" << raft_drv->get_committed_seqno()
                 << "\n";
-    #endif
+#endif
     auto end_ts = get_timestamp_ns();
     latencies.insert(
       std::make_pair(reqs_no, metadata(client_req_id, (end_ts - now_ts))));
     sum_latency += (end_ts - now_ts);
-    count ++;
-    if (client_req_id % 10000 == 0) {  
-      std::cout << "*====RAFT====* " << __func__ 
+    count++;
+    if (client_req_id % 10000 == 0)
+    {
+      std::cout << "*====RAFT====* " << __func__
                 << " client_req_id=" << client_req_id
                 << " zil_blk_id=" << zil_blk_id
                 << " committed_seqno=" << raft_drv->get_committed_seqno()
-                << " latency (ns)=" << (end_ts - now_ts) <<  " avg_lat = " << (sum_latency/(1.0*count)) << " ns\n";
+                << " latency (ns)=" << (end_ts - now_ts)
+                << " avg_lat = " << (sum_latency / (1.0 * count)) << " ns\n";
     }
   }
   else
@@ -227,7 +226,6 @@ void ccf_replication(
     assert(false);
   }
 }
-
 
 void ccf_replication_blocking(
   std::weak_ptr<void> driver,
@@ -250,7 +248,7 @@ void ccf_replication_blocking(
     auto data_to_replicate = std::make_shared<std::vector<uint8_t>>(sz_data);
     ::memcpy(data_to_replicate->data(), data, sz_data);
     std::shared_ptr<RaftDriver> raft_drv =
-    std::static_pointer_cast<RaftDriver>(drv_shared);
+      std::static_pointer_cast<RaftDriver>(drv_shared);
 
     reqs_no++;
     if (reqs_no % 50000 == 0)
@@ -281,20 +279,20 @@ void ccf_replication_blocking(
     }
     auto now_ts = get_timestamp_ns();
 
-    if (reqs_no % 10000 ==0) {
-      std::cout << __PRETTY_FUNCTION__
-                << " reqs_no=" << reqs_no
+    if (reqs_no % 10000 == 0)
+    {
+      std::cout << __PRETTY_FUNCTION__ << " reqs_no=" << reqs_no
                 << " committed_seqno=" << raft_drv->get_committed_seqno()
                 << "\n";
-   }
-   #if 0
+    }
+#if 0
    std::cout << __PRETTY_FUNCTION__
                 << "Before replicate_commitable: reqs_no=" << reqs_no
                 << " committed_seqno=" << raft_drv->get_committed_seqno()
                 << "\n";
-   
-    #endif
-     raft_drv->replicate_commitable("2", data_to_replicate, 0);
+
+#endif
+    raft_drv->replicate_commitable("2", data_to_replicate, 0);
     // wait until committed
     while (raft_drv->get_committed_seqno() < reqs_no)
     {
@@ -305,16 +303,18 @@ void ccf_replication_blocking(
                 << "\n";
 #endif
     }
-    
+
     auto end_ts = get_timestamp_ns();
     latencies.insert(
       std::make_pair(reqs_no, metadata(client_req_id, (end_ts - now_ts))));
     sum_latency += (end_ts - now_ts);
-    count ++;
-    if (count % 1000 == 0) {  
+    count++;
+    if (count % 1000 == 0)
+    {
       std::cout << __PRETTY_FUNCTION__ << " reqs_no=" << reqs_no
-              << " committed_seqno=" << raft_drv->get_committed_seqno()
-              << " latency (ns)=" << (end_ts - now_ts) <<  " avg_lat = " << (sum_latency/(1.0*count)) << "\n";
+                << " committed_seqno=" << raft_drv->get_committed_seqno()
+                << " latency (ns)=" << (end_ts - now_ts)
+                << " avg_lat = " << (sum_latency / (1.0 * count)) << "\n";
     }
   }
   else
@@ -393,7 +393,8 @@ static void apply_cmds(std::shared_ptr<RaftDriver> driver)
           __func__,
           src_node_str,
           driver->get_committed_seqno(),
-          reqs_nb.load(), data_sz);
+          reqs_nb.load(),
+          data_sz);
 #endif
     }
     else if (data_sz == 0)
@@ -429,6 +430,51 @@ static void listen_for_acks(std::shared_ptr<RaftDriver> driver, int node_id)
       return;
     */
   }
+}
+
+static void create_server_thread(
+  int i,
+  const char* private_key_file,
+  const char* cert_file,
+  const char* addr,
+  const char* port,
+  std::shared_ptr<RaftDriver> driver, 
+  std::barrier<std::__empty_completion>& sync_point)
+{
+  // Create a new event loop for this thread
+  struct ev_loop* loop = ev_loop_new(EVFLAG_AUTO);
+  
+  TLSServerContext tls_ctx;
+
+  if (tls_ctx.init(private_key_file, cert_file, AppProtocol::H3) != 0)
+  {
+    exit(EXIT_FAILURE);
+  }
+  Server* s = new Server(loop, tls_ctx, i);
+
+#warning "NON BLOCKING REPLICATION"
+  std::shared_ptr<ccf_callbacks_set> ptr_callable =
+    std::make_shared<ccf_callbacks_set>(
+      std::static_pointer_cast<void>(driver),
+      ccf_replication,
+      ccf_committed_seqno);
+  s->register_ccf_functions(ptr_callable);
+  int port_num = std::stoll(port) + i;
+  std::string port_str = std::to_string(port_num);
+  s->init(addr, port_str.c_str());
+  s->assign_server_id(i);
+  if (i == 0)
+  {
+    ccf_monitor_ptr->set_quic_server(s);
+  }
+  sync_point.arrive_and_wait();
+  std::cout << __PRETTY_FUNCTION__ << " i=" << i << "\n";
+  ev_run(loop, 0);
+
+  s->disconnect();
+  s->close();
+  ev_loop_destroy(loop);
+  delete(s);
 }
 
 int main(int argc, char* argv[])
@@ -514,7 +560,8 @@ int main(int argc, char* argv[])
       std::map<std::string, ccf::kv::Configuration::NodeInfo>{
         std::make_pair(
           std::to_string(primary_node),
-          ccf::kv::Configuration::NodeInfo(primary_ip, std::to_string(primary_listening_port))),
+          ccf::kv::Configuration::NodeInfo(
+            primary_ip, std::to_string(primary_listening_port))),
         std::make_pair(
           std::to_string(follower_1),
           ccf::kv::Configuration::NodeInfo(
@@ -546,94 +593,35 @@ int main(int argc, char* argv[])
 #endif
     fmt::print("{} QUIC server\n", __PRETTY_FUNCTION__);
     // =============== QUIC
+    int no_servers = 128;
 
-    ccf_monitor_ptr = std::make_unique<ccf_monitor>();
-    Server s(EV_DEFAULT, tls_ctx);
-   
-    s.init(addr, port);
-    s.assign_server_id(5);
+    ccf_monitor_ptr = std::make_unique<ccf_monitor>(no_servers);
+    std::vector<std::thread> threads;
+    std::barrier sync_point(no_servers+1);
 
-#ifdef STANDALONE
-#  warning "STANDALONE BENCHMARK"
-    std::shared_ptr<ccf_callbacks_set> ptr_callable =
-      std::make_shared<ccf_callbacks_set>(
-        driver, empty_func);
-    s.register_ccf_functions(ptr_callable);
-#else
-#ifdef BLOCKING_REPLICATION
-#warning "BLOCKING REPLICATION"
-    std::shared_ptr<ccf_callbacks_set> ptr_callable =
-      std::make_shared<ccf_callbacks_set>(driver, ccf_replication_blocking);
-      s.register_ccf_functions(ptr_callable);
-
-#else
-#warning "NON BLOCKING REPLICATION"
-    std::shared_ptr<ccf_callbacks_set> ptr_callable =
-      std::make_shared<ccf_callbacks_set>(driver, ccf_replication, ccf_committed_seqno);
-    s.register_ccf_functions(ptr_callable);
-#endif
-#endif
-    ccf_monitor_ptr->set_quic_server(&s);
+    for (auto i = 0; i < no_servers; i++)
+    {
+      threads.emplace_back(
+        create_server_thread,
+        i,
+        private_key_file,
+        cert_file,
+        addr,
+        port,
+        driver, 
+        std::ref(sync_point));
+    }
+    sync_point.arrive_and_wait();
+    std::cout << __PRETTY_FUNCTION__ << " all threads done in std::barrier\n";
     ccf_monitor_ptr->started.store(true);
 
-    ev_run(EV_DEFAULT, 0);
 
-    s.disconnect();
-    s.close();
-    // =============== QUIC end
+    for (auto& t : threads)
+      t.join();
 
-    // this is because we send an AppendEntries message every time we
-    // send a new_configuration
-    for (auto i = 0ULL; i < k_num_requests; i++)
-    {
-      // std::unique_lock<std::mutex> tmp_l(leader_mtx);
+      fmt::print("{} SHOULD NOT REACH HERE\n", __PRETTY_FUNCTION__);
+  // =============== QUIC end
 
-      // fmt::print("{}:{} ---> replicate_commitable()\n", __func__,
-      // socket_layer::get_thread_id());
-      driver->replicate_commitable("2", data, 0);
-      if (i == 0)
-      {
-        threads_leader.emplace_back(
-          std::thread(listen_for_acks, driver, follower_1));
-        threads_leader.emplace_back(
-          std::thread(listen_for_acks, driver, follower_2));
-      }
-#if 0
-      acks += driver->periodic_listening_acks(std::to_string(follower_1));
-      if (acks % 50000 == 0)
-        fmt::print("{} acks={}\n", __func__, acks);
-#endif
-    }
-    fmt::print("{} --> finished, 1\n", __func__);
-    for (;;)
-    {
-#if 1
-      fmt::print(
-        "{} --> get_committed_seqno()={}\n",
-        __func__,
-        driver->get_committed_seqno());
-#endif
-      if (driver->get_committed_seqno() == k_num_requests)
-        break;
-      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
-
-    leader_end = std::chrono::high_resolution_clock::now();
-    fmt::print(
-      "{} ---> taken timestamp={}s\n", __func__, driver->get_committed_seqno());
-
-    threads_leader[0].join();
-    threads_leader[1].join();
-
-    driver->replicate_commitable("2", data, 0);
-
-    acks += driver->periodic_listening_acks(std::to_string(follower_1));
-    acks += driver->periodic_listening_acks(std::to_string(follower_2));
-
-    // threads_leader[0].join();
-    driver->close_connections(std::to_string(primary_node));
-    driver->close_connections(std::to_string(follower_1));
-    driver->close_connections(std::to_string(follower_2));
   }
   else
   {
