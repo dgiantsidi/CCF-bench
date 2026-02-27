196505b5981dd8e1e21e91fa597d7281aab16cbf
dgiantsidi <dimitra.giantsidi@gmail.com>
2026-02-27 22:23:38 +0000

    updates on the IPs

--- src/consensus/aft/test/config.hpp
+++ src/consensus/aft/test/config.hpp
@@ -14,12 +14,12 @@ constexpr int payload_sz_entry = sizeof(size_t) + sizeof(bool) +
 #ifdef REGULAR_VM
 #  warning "R-VMs running here"
 constexpr int primary_node = 0;
-std::string primary_ip = "10.5.0.7"; // 10.5.0.7";
+std::string primary_ip = "172.29.137.46"; // 10.5.0.7";
 constexpr int primary_listening_port = 1800;
 constexpr int primary_sending_port = 1900;
 
 constexpr int follower_1 = 1;
-std::string follower_1_ip = "10.5.0.5"; //"10.5.0.5"; //"10.1.0.4";
+std::string follower_1_ip = "172.29.135.147"; //"10.5.0.5"; //"10.1.0.4";
 constexpr int follower_1_listening_port = 2800;
 constexpr int follower_1_sending_port = 2900;
 
@@ -29,12 +29,12 @@ constexpr int follower_2_listening_port = 3800;
 constexpr int follower_2_sending_port = 3900;
 #else
 constexpr int primary_node = 0;
-std::string primary_ip = "172.18.0.9"; // 10.5.0.7";
+std::string primary_ip = "172.29.137.46"; // 10.5.0.7";
 constexpr int primary_listening_port = 1800;
 constexpr int primary_sending_port = 1900;
 
 constexpr int follower_1 = 1;
-std::string follower_1_ip = "172.18.0.8"; //"10.5.0.5"; //"10.1.0.4";
+std::string follower_1_ip = "172.29.135.147"; //"10.5.0.5"; //"10.1.0.4";
 constexpr int follower_1_listening_port = 2800;
 constexpr int follower_1_sending_port = 2900;
 
